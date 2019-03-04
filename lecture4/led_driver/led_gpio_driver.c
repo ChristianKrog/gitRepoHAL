2b462d563f42fb81b2a51f42171e7122745983dc
Your Name <you@example.com>
2019-03-04 15:02:26 +0100

    Driver done

--- lecture4/led_driver/led_gpio_driver.c
+++ lecture4/led_driver/led_gpio_driver.c
@@ -5,76 +5,119 @@
  #include <linux/uaccess.h>
  #include <linux/module.h>
 
-#define ADC_MAJOR = 64
-#define ADC_MINOR = 0
-#define ADC_NBR_MINORS = 8// 8-ch ADC
+#define LED2_GPIO 21
+#define LED_MAJOR 64
+#define LED_MINOR 1
+#define LED_AMOUNT 1
 
 static struct cdev led_cdev;
 struct file_operations led_fops;
+int devno;
 
 MODULE_LICENSE("GPL");
 
- static int led3gpio_init(void)
+ static int led2gpio_init(void)
  { 
- 	int gpio_request(21,"LED3");
-
- /* set as input or output, returning 0 or negative errno */
-	int gpio_direction_output(21, 0);
-
-
-/* _______________________KIG PÅ NEDENDSTÅENDE________________*/
-	
 	int err = 0; 
-	err = try_to_acquire_my_specific_ressource();
-
-	if(err< 0) 
+	
+	err = gpio_request(LED2_GPIO,"LED2");
+	if(err < 0) 
 	{
-		gotoerr_exit;
+		goto err_exit;
 	}
+	printk("gpio_reguest was successfull\n");
 
-	devno=MKDEV(ADC_MAJOR, ADC_MINOR);
-	err=register_chrdev_region(&devno, ADC_NBR_MINOR,”ads7870”);
-		
-	if(err< 0)// Acquiremajor/minor
+	err = gpio_direction_output(LED2_GPIO, 1);
+	if(err < 0)
 	{
-		gotoerr_free_buf;
+		goto err_free_gpio;
 	}
+	printk("gpio_direction for LED2 got set to output\n");
+
+	devno=MKDEV(LED_MAJOR, LED_MINOR);
+	err=register_chrdev_region(devno, LED_AMOUNT, "LED2");
+	if(err < 0)// Acquiremajor/minor
+	{
+		goto err_free_chrdev_region;
+	}
+	printk("chrdev_region registered\n");
 
 	cdev_init(&led_cdev, &led_fops);
-    err = cdev_add(&led_cdev, devno, no_devices);
+    err = cdev_add(&led_cdev, devno, LED_AMOUNT);
     if(err< 0)// Register driver with kernel
     {
-		gotoerr_dev_unregister;
+		goto err_dev_unregister;
     }
+    printk("cdev_add successfull\n");
+	printk("LED GPIO driver got Rhino'd\n");
         
     return 0; // Success!!!
 
-    err_dev_unregister:
-	unregister_chrdev_region(devno, nbr_devices);
+	err_free_gpio:
+	gpio_free(LED2_GPIO);
 
-	err_free_buf:
-	// Release resourceswhichwereacquired
-		
+	err_free_chrdev_region:
+	gpio_free(LED2_GPIO);
+
+	err_dev_unregister:
+	unregister_chrdev_region(devno, LED_AMOUNT);
+
 	err_exit:
- 		
- 	return err;
-	
-	
-/*_________________KIG PÅ OVENSTÅENDE____________________*/
-
-
- 	printk(KERN_ALERT "LED GPIO driver got Rhino'd\n");
+	return err;
  }
 
-static void led3gpio_exit(void)
+static void led2gpio_exit(void)
  {
- // Delete Cdev
- // Unregister Device
- // Free GPIO
- 	 /* release previously-claimed GPIO */
-	void gpio_free(unsigned 21);
-	printk(KERN_ALERT "LED GPIO driver got UnRhino'd\n");
+ 	cdev_del(&led_cdev);
+ 	printk("cdev deleted\n");
+ 	unregister_chrdev_region(devno, LED_AMOUNT);
+ 	printk("chrdev_region unregistered\n");
+ 	gpio_free(LED2_GPIO);
+ 	printk("gpio got free'd\n");
+	printk("LED GPIO driver got UnRhino'd\n");
  }
 
-module_init(led3gpio_init);
-module_exit(led3gpio_exit);
+ int led2gpio_open(struct inode *inode, struct file *filep)
+ {
+ 	int major, minor;
+
+ 	major = MAJOR(inode->i_rdev);
+	minor = MINOR(inode->i_rdev);
+ 	printk("Opening MyGpio Device [major], [minor]: %i, %i\n", major, minor);
+ 	return 0;
+ }
+
+ int led2gpio_release(struct inode *inode, struct file *filep)
+ {
+	int minor, major;
+
+ 	major = MAJOR(inode->i_rdev);
+ 	minor = MINOR(inode->i_rdev);
+ 	printk("Closing/Releasing MyGpio Device [major], [minor]: %i, %i\n", major, minor);
+
+ return 0;
+ }
+
+ //KIG PÅ NEDENSTÅENDE 
+ //
+ //
+
+  ssize_t led2gpio_write(struct file *filep, const char __user *ubuf, size_t count, loff_t *f_pos)
+ {
+ 	int write_value = 0;
+    sscanf(ubuf, "%d", &write_value);
+    gpio_set_value(LED2_GPIO, write_value);
+
+ return count;
+ }
+
+ struct file_operations led_fops = 
+ {
+	.owner = THIS_MODULE,
+	.write = led2gpio_write,
+	.open = led2gpio_open,
+	.release = led2gpio_release,
+};
+
+module_init(led2gpio_init);
+module_exit(led2gpio_exit);
