3337a673f1c2afce3fc3e88279effa45b79a1ad2
Your Name <you@example.com>
2019-04-16 13:53:36 +0200

    working sweet. but needs last touch

--- lecture8/attr_drv.c
+++ lecture8/attr_drv.c
@@ -13,6 +13,8 @@
 #include <linux/of.h>
 #include <linux/timer.h>
 
+#define DEBUG_FLAG 1
+
 static int NUM_OF_BUTS = 0;
 
 MODULE_LICENSE("GPL");
@@ -36,12 +38,11 @@ static void timer_funct(unsigned long funct_parameter);
 
 static ssize_t rhino_led_state_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	printk(KERN_DEBUG "rhino_led_state_show got called");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_state_show got called");
 	int curr;
 	struct button_dev *d = dev_get_drvdata(dev);
 	curr = gpio_get_value(d->gpio_number);
-
-	int len = sprintf(buf, "%d", curr);
+	int len = sprintf(buf, "%d\n", curr);
 	return len;
 }
 
@@ -57,7 +58,7 @@ static ssize_t rhino_led_state_store(struct device *dev, struct device_attribute
 	}
 	rhino_button_devs[d->buttonno].default_output = (int)curr;
 	gpio_set_value(d->gpio_number, (int)curr);
-	printk(KERN_DEBUG "rhino_led_state_store got called, and have set gpio%d to %u\n", d->gpio_number, curr);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_state_store got called, and have set gpio%d to %u\n", d->gpio_number, curr);
 	return size;
 }
 
@@ -74,7 +75,7 @@ static ssize_t rhino_led_toggle_store(struct device *dev, struct device_attribut
 	}
 	d->state = incomming;
 	rhino_button_devs[d->buttonno].state = incomming;
-	printk(KERN_DEBUG "rhino_led_toggle_store got called, and have set the state of gpio%d to %u\n", d->gpio_number, incomming);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_toggle_store got called, and have set the state of gpio%d to %u\n", d->gpio_number, incomming);
 	
 	return size;
 }
@@ -82,16 +83,13 @@ static ssize_t rhino_led_toggle_store(struct device *dev, struct device_attribut
 static ssize_t rhino_led_toggle_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	struct button_dev *d = dev_get_drvdata(dev);
-	int size = sprintf(buf, "%d", d->state);
-	printk(KERN_DEBUG "rhino_led_toggle_show got called, and printet the state of gpio%d to %u\n", d->gpio_number, d->state);
+	int size = sprintf(buf, "%d\n", d->state);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_toggle_show got called, and printet the state of gpio%d to %u\n", d->gpio_number, d->state);
 	return size;
 }
 
 static ssize_t rhino_led_delay_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
 {
-	
-	
-
 	struct button_dev *d = dev_get_drvdata(dev);
 	int err = kstrtou8(buf, 0, &d->timeout_in_sec);
 	if (err < 0) 
@@ -101,28 +99,18 @@ static ssize_t rhino_led_delay_store(struct device *dev, struct device_attribute
 	}
 	rhino_button_devs[d->buttonno].timeout_in_sec = d->timeout_in_sec;
 
-
-	if(d->timer_flag == 0)
-	{
-		del_timer(&rhino_timer[d->buttonno]);
-		init_timer(&rhino_timer[d->buttonno]);
-	}
-
 	rhino_timer[d->buttonno].expires = (jiffies + d->timeout_in_sec * HZ);
 
 	rhino_timer[d->buttonno].function = timer_funct;
 
 	rhino_timer[d->buttonno].data = (unsigned long)d;
 
-	printk(KERN_DEBUG "timer->data = %lu\n", rhino_timer[d->buttonno].data);
+	if(d->timer_flag == 0)
+	{
+		add_timer(&rhino_timer[d->buttonno]);
+	}
 
-	int t = d->buttonno;
-
-	printk(KERN_DEBUG "d->buttonno%d\n = ", t);
-
-	add_timer(&rhino_timer[t]); ///////////////////////////////7
-
-	printk(KERN_DEBUG "rhino_led_delay_store got called, and have set delay of set gpio%d to %u\n", d->gpio_number, d->timeout_in_sec);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_delay_store got called, and have set delay of set gpio%d to %u\n", d->gpio_number, d->timeout_in_sec);
 
 	rhino_button_devs[d->buttonno].timer_flag = 1;
 	
@@ -132,8 +120,8 @@ static ssize_t rhino_led_delay_store(struct device *dev, struct device_attribute
 static ssize_t rhino_led_delay_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	struct button_dev *d = dev_get_drvdata(dev);
-	int size = sprintf(buf, "%d", d->timeout_in_sec);
-	printk(KERN_DEBUG "rhino_led_delay_show got called, and delay of gpio%d printed with a value of %u\n", d->gpio_number, d->timeout_in_sec);
+	int size = sprintf(buf, "%d\n", d->timeout_in_sec);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "rhino_led_delay_show got called, and delay of gpio%d printed with a value of %u\n", d->gpio_number, d->timeout_in_sec);
 	return size;
 }
 
@@ -147,7 +135,6 @@ static struct attribute *rhino_led_attrs[] = {
   &dev_attr_rhino_led_state.attr,
   &dev_attr_rhino_led_toggle.attr,
   &dev_attr_rhino_led_delay.attr,
-  // More attributes could go in here, 
   NULL, 
   }; 
 
@@ -165,7 +152,7 @@ unsigned int rhino_custom;
 
 static int rhino_button_probe(struct platform_device *pdev)
 {
-	printk(KERN_DEBUG "New Platform device: %s \n", pdev->name);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "New Platform device: %s \n", pdev->name);
 	int err = 0, i, k;
 
 	struct device *dev = &pdev->dev;
@@ -173,12 +160,15 @@ static int rhino_button_probe(struct platform_device *pdev)
 	enum of_gpio_flags flag;
 
 	NUM_OF_BUTS = of_gpio_count(np);
-	//const char name[30] = "rhino_custom\0";
 	err = of_property_read_u32(np, "rhino_custom", &rhino_custom); 
+	
 	if(err != 0) printk(KERN_ALERT "of_property_read_u32 failed with error: %d\n", err);
-	else printk(KERN_DEBUG "rhino_custom = %d\n", rhino_custom);
-
-	printk(KERN_DEBUG "gpio count: %d\n", NUM_OF_BUTS);
+	
+	if(DEBUG_FLAG)
+	{
+		printk(KERN_DEBUG "rhino_custom = %d\n", rhino_custom);
+		printk(KERN_DEBUG "gpio count: %d\n", NUM_OF_BUTS);
+	}
 
 	for(k = 0; k < NUM_OF_BUTS; k++)
 	{
@@ -206,43 +196,45 @@ static int rhino_button_probe(struct platform_device *pdev)
 		if(rhino_button_devs[i].dir == 0)
 		{
 			err = gpio_direction_input(rhino_button_devs[i].gpio_number);
-			if(err == 0)printk(KERN_DEBUG "gpio %d directions got set to input\n", rhino_button_devs[i].gpio_number);
-			else 
+			if(DEBUG_FLAG)printk(KERN_DEBUG "gpio %d directions got set to input\n", rhino_button_devs[i].gpio_number);
+			if(err < 0) 
 			{
 				printk(KERN_ALERT "gpio_direction_input error in gpio: %d\n", rhino_button_devs[i].gpio_number);
-		goto error_exit;
-		}
+				goto error_exit;
+			}
+
 		}
 		else if(rhino_button_devs[i].dir == 1)
 		{
-		err = gpio_direction_output(rhino_button_devs[i].gpio_number, rhino_button_devs[i].default_output);	
+			err = gpio_direction_output(rhino_button_devs[i].gpio_number, rhino_button_devs[i].default_output);	
 		
-		if(err == 0)
-		{
-			printk(KERN_DEBUG "gpio %d directions got set to output\n", rhino_button_devs[i].gpio_number);
-		
-			if(((rhino_custom >> i) & 1) > 0)
-                        {
-                        	gpio_set_value(rhino_button_devs[i].gpio_number, 1);
-                        	printk(KERN_DEBUG "gpio %d got set to high\n", rhino_button_devs[i].gpio_number);
-			}
-                	else
-                	{
-				gpio_set_value(rhino_button_devs[i].gpio_number, 0);
-                        	printk(KERN_DEBUG "gpio%d got set to low\n", rhino_button_devs[i].gpio_number);
-                	}
-		}	
+			if(err == 0)
+			{
+				if(DEBUG_FLAG)printk(KERN_DEBUG "gpio %d directions got set to output\n", rhino_button_devs[i].gpio_number);
+			
+				if(((rhino_custom >> i) & 1) > 0)
+	            {
+	        		gpio_set_value(rhino_button_devs[i].gpio_number, 1);
+
+	        		if(DEBUG_FLAG)printk(KERN_DEBUG "gpio %d got set to high\n", rhino_button_devs[i].gpio_number);
+				}
+	        	else
+	        	{
+					gpio_set_value(rhino_button_devs[i].gpio_number, 0);
+					if(DEBUG_FLAG)printk(KERN_DEBUG "gpio%d got set to low\n", rhino_button_devs[i].gpio_number);
+	        	}
+			}	
 		else 
 		{
-		printk(KERN_ALERT "gpio_direction_output error in gpio: %d\n", rhino_button_devs[i].gpio_number);
-		goto error_exit;
+			printk(KERN_ALERT "gpio_direction_output error in gpio: %d\n", rhino_button_devs[i].gpio_number);
+			goto error_exit;
 		}
 		}
 		rhino_button[i] = device_create(rhino_buttons, NULL, MKDEV(MAJOR(devno), i), &rhino_button_devs[i], "button%d", i); 
 
 	}	
 	
-	printk(KERN_DEBUG "gpio_reguest, successfull\n");
+	printk(KERN_DEBUG "gpio initialisation, successfull\n");
 
 	return err;
 	error_exit:
@@ -255,14 +247,14 @@ static int rhino_button_probe(struct platform_device *pdev)
 
 static int rhino_button_remove(struct platform_device *pdev)
 {
-printk(KERN_DEBUG "Removing device %s \n", pdev->name);
-for(int i = 0; i < NUM_OF_BUTS; i++)
-{
-	del_timer(&rhino_timer[i]);
-	device_destroy(rhino_buttons, MKDEV(MAJOR(devno), i));
-	gpio_free(rhino_button_devs[i].gpio_number);
-}
-return 0;
+	printk(KERN_DEBUG "Removing device %s \n", pdev->name);
+	for(int i = 0; i < NUM_OF_BUTS; i++)
+	{
+		del_timer(&rhino_timer[i]);
+		device_destroy(rhino_buttons, MKDEV(MAJOR(devno), i));
+		gpio_free(rhino_button_devs[i].gpio_number);
+	}
+	return 0;
 }
 
 static const struct of_device_id rhino_button_platform_device_match[] =
@@ -271,12 +263,12 @@ static const struct of_device_id rhino_button_platform_device_match[] =
 };
 
 static struct platform_driver rhino_button_platform_driver = {
-.probe = rhino_button_probe,
-.remove = rhino_button_remove,
-.driver = {
-	.name = "ase, plat_drv",
-	.of_match_table = rhino_button_platform_device_match,
-	.owner = THIS_MODULE, },
+	.probe = rhino_button_probe,
+	.remove = rhino_button_remove,
+	.driver = {
+		.name = "ase, plat_drv",
+		.of_match_table = rhino_button_platform_device_match,
+		.owner = THIS_MODULE, },
 };
 
 
@@ -303,7 +295,6 @@ ssize_t gpio_write(struct file *filep, const char __user *buf, size_t count, lof
   	char rhino_buf[2];
   	int err = copy_from_user(rhino_buf, buf, count);
   	if(err !=0) printk(KERN_ALERT "Write error!");
-
 	sscanf(rhino_buf, "%d", &out_val);
   	gpio_set_value(rhino_button_devs[nodno].gpio_number, out_val);
   	*f_pos += count;
@@ -312,22 +303,22 @@ ssize_t gpio_write(struct file *filep, const char __user *buf, size_t count, lof
 
  int gpio_open(struct inode *inode, struct file *filep)
  {
- int major, minor;
- major = MAJOR(inode->i_rdev);
- minor = MINOR(inode->i_rdev);
- printk(KERN_DEBUG "Opening gpio Device [major], [minor]: %i, %i\n", major, minor);
- return 0;
+	 int major, minor;
+	 major = MAJOR(inode->i_rdev);
+	 minor = MINOR(inode->i_rdev);
+	 if(DEBUG_FLAG)printk(KERN_DEBUG "Opening gpio Device [major], [minor]: %i, %i\n", major, minor);
+	 return 0;
  }
 
  int gpio_release(struct inode *inode, struct file *filep)
  {
- int minor, major;
+	 int minor, major;
 
- major = MAJOR(inode->i_rdev);
- minor = MINOR(inode->i_rdev);
- printk(KERN_DEBUG "Releasing gpio Device [major], [minor]: %i, %i\n", major, minor);
+	 major = MAJOR(inode->i_rdev);
+	 minor = MINOR(inode->i_rdev);
+	 if(DEBUG_FLAG)printk(KERN_DEBUG "Releasing gpio Device [major], [minor]: %i, %i\n", major, minor);
 
- return 0;
+	 return 0;
  }
 
 
@@ -345,7 +336,7 @@ static int gpio_init(void)
 	int err;
 	err = alloc_chrdev_region(&devno, 0,255, "Buttons");
 	if(err < 0) goto error_free_chrdev_region;
-	printk(KERN_DEBUG "chrdev_region got registered\n");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "chrdev_region got registered\n");
 
 	rhino_buttons = class_create(THIS_MODULE, "rhino_buttons_cls");
 	rhino_buttons->dev_groups = rhino_led_groups;
@@ -354,8 +345,11 @@ static int gpio_init(void)
 	
 	err = cdev_add(&rhino_cdev, devno, 255);
 	if(err < 0) goto error_free_chrdev_region;
-	printk(KERN_DEBUG "cdev got add'ed\n");
-	printk(KERN_DEBUG "init got executed\n");
+	if(DEBUG_FLAG)
+	{
+		printk(KERN_DEBUG "cdev got add'ed\n");
+		printk(KERN_DEBUG "init got executed\n");
+	}
 
 	platform_driver_register(&rhino_button_platform_driver);
 
@@ -369,14 +363,14 @@ static int gpio_init(void)
 static void gpio_exit(void)
 {
 	platform_driver_unregister(&rhino_button_platform_driver);
-	printk(KERN_DEBUG "Platform driver got unregistered\n");
-	class_destroy(rhino_buttons);
-	printk(KERN_DEBUG "Rhino class got destroyed\n");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "Platform driver got unregistered\n");
 	cdev_del(&rhino_cdev);
-	printk(KERN_DEBUG "cdev got deleted\n");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "cdev got deleted\n");
+	class_destroy(rhino_buttons);
+	if(DEBUG_FLAG)printk(KERN_DEBUG "Rhino class got destroyed\n");
 	unregister_chrdev_region(devno, 255);
-	printk(KERN_DEBUG "chrdev_region got unregistered\n");
-	printk(KERN_DEBUG "gpiodriver got Unrhino'd\n");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "chrdev_region got unregistered\n");
+	if(DEBUG_FLAG)printk(KERN_DEBUG "gpiodriver got Unrhino'd\n");
 }
 
 module_init(gpio_init);
@@ -385,7 +379,7 @@ module_exit(gpio_exit);
 static void timer_funct(unsigned long funct_parameter)
 {
 	struct button_dev * a = (struct button_dev *) funct_parameter;
-	printk("funct_parameter: %p\n", a);
+
 	rhino_timer[a->buttonno].expires = jiffies + a->timeout_in_sec * HZ;
 
 	add_timer(&rhino_timer[a->buttonno]);
